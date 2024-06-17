0c7d9af7e118d6ec555221f897aa1f63558fb79e
jacknicklenson <jacknicklenson59@gmail.com>
2024-06-17 13:09:20 +0300

    add 10M & 100M particle benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 files=('simple.zig' 'SoA_MT.zig' 'SoA.zig' 'SoA_MT_SIMD.zig' 'SoA_SIMD.zig')
-iters=('100' '1000' '10000' '100000' '1000000')
+iters=('100' '1000' '10000' '100000' '1000000' '10000000' '100000000')
 
 for particle_sz in "${iters[@]}"; do
 	echo "=================================================== PARTICLE SIZE: $particle_sz   ======================================================================="
